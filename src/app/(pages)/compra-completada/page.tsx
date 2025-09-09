5e1440bd5a4c28416cf87b701643193d6c2e17eb
Emerson Fernadez <escanorrosa123@gmail.com>
2025-09-09 10:02:32 -0500

    Zxzx

--- src/app/(pages)/compra-completada/page.tsx
+++ src/app/(pages)/compra-completada/page.tsx
@@ -5,7 +5,7 @@ import DatosCompra from "./components/DatosCompra";
 
 export default function Page() {
   return (
-    <main className="w-screen min-h-screen  h-full  background-login max-sm:h-auto max-sm:py-10 ">
+    <main className="w-screen min-h-screen h-max   background-login  max-md:h-auto">
       <div className="w-full min-h-screen h-full  max-w-[1440px] mx-auto flex    p-12 max-md:flex-col-reverse  max-md:p-4 max-md:py-10 max-md:gap-10 max-md:h-auto">
         <PsAi />
         <DatosCompra />
