1f7cf73806ea12bed58a56bda02848821c07d0ec
Emerson Fernadez <escanorrosa123@gmail.com>
2025-11-17 09:28:14 -0500

    asas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-"use client";
 import { Toaster } from "sonner";
 import "@/app/globals.css";
 import "swiper/css";
@@ -7,13 +6,20 @@ import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { Providers } from "./providers";
 
+export const metadata = {
+  title: "Team Crafter App",
+  icons: {
+    icon: "/cropped-Favicon-180x180.png",
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="ligth h-full">
+    <html lang="en" className="light h-full">
       <body>
         <Providers>{children}</Providers>
         <Toaster richColors />
